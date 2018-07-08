999b3368521d3bb178226709061bda3f6380f510
Rafael Escaleira <40582668+rafaelescaleira@users.noreply.github.com>
2018-07-08 12:56:50 -0400

    Contém função de teste

---
+++ teste_rb.c
@@ -0,0 +1,19 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                               *
+ * Nome : Rafael Escaleira Ferreira dos Santos.  *
+ * Universidade Federal do Mato Grosso do Sul    *
+ * Disciplina : Estrutura de Dados e Programação *
+ *                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+#include "rb.h"
+
+int main (void) {
+
+    confere_funcionamento ();
+    teste_tempo_execucao (1000000);
+
+    return 0;
+
+}
